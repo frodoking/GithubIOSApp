4366709271b6022e5ff0fc404fd2643740ed1569
frodoking <awangyun8@gmail.com>
2015-11-05 17:36:55 +0800

    change discovery& more tab to static tableview

--- Github/Core/Module/More/MoreTableViewController.swift
+++ Github/Core/Module/More/MoreTableViewController.swift
@@ -12,9 +12,7 @@ import Alamofire
 class MoreTableViewController: UITableViewController {
     
     var currentLogin: String?
-    var currentAvatarUrl: String?
-    
-    var items = ["Login", "About", "Feedback", "Logout"]
+    var currentAvatarUrl: String? 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,94 +31,5 @@ class MoreTableViewController: UITableViewController {
         currentLogin = NSUserDefaults.standardUserDefaults().objectForKey(Key.User.CurrentLogin)?.stringValue
         currentAvatarUrl = NSUserDefaults.standardUserDefaults().objectForKey(Key.User.CurrentAvatarUrl)?.stringValue
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        if (currentLogin != nil) {
-            return items.count
-        } else {
-            return items.count - 1
-        }
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 1
-    }
-
- 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(Key.CellReuseIdentifier.MoreCell, forIndexPath: indexPath)
-
-        // Configure the cell...
-        if (indexPath.section == 0 && currentLogin != nil) {
-                cell.textLabel!.text = currentLogin
-                Alamofire.request(.GET, currentAvatarUrl!)
-                    .responseData { response in
-                        let imageData = UIImage(data: response.data!)
-                        cell.imageView?.image = imageData
-                }
-        } else {
-            cell.textLabel!.text = items[indexPath.section]
-        }
-        
-        if (indexPath.section == 3 && currentLogin != nil) {
-            cell.textLabel!.text = items[3]
-        }
-
-        return cell
-    }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
